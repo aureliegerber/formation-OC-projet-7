6afe41eaa6513abf7cccdb2da3e8ac424dfc0a61
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:10:48 +0100

    feat: add the home page

---
+++ src/pages/Home/home.js
@@ -0,0 +1,21 @@
+import Header from "../../components/Header/header";
+import Banner from "../../components/Banner/banner";
+import Gallery from "../../components/Gallery/gallery";
+import Footer from "../../components/Footer/footer";
+import styles from "./home.module.css"
+
+
+function Home() {
+  return (
+    <div>
+      <div className={styles.container}>
+        <Header />
+        <Banner />     
+        <Gallery />
+      </div>
+      <Footer />  
+    </div>
+  );
+}
+
+export default Home;
