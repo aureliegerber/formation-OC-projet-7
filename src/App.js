d4e7a1d44c5e65f3ce6225a2c99c673d157d6741
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:14:14 +0100

    feat: create the Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import About from "./pages/About/about";
+import Error from "./pages/Error/error";
+import Lodging from "./pages/Lodging/lodging";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>      
+            <Routes>        
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />      
+                <Route path="/lodging/:id" element={<Lodging />} />      
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
