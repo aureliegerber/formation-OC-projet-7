d8ed41cc238f899be358ac965f3520829461cbf2
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:05:09 +0100

    feat: add the component Footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import styles from "./footer.module.css"
+import logo from "../../assets/logo-footer.png"
+
+function Footer() {
+	return (
+		<footer className={styles.footer}>
+			<img src={logo} alt="Kasa - Agence de location d'appartements entre particuliers" />
+			<p className={styles.footer__copyright}>© 2020 Kasa. All rights reserved</p>
+		</footer>
+	)
+}
+
+export default Footer;
