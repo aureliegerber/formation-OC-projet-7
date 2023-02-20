79162170d30064504c4c6e40b9d186bdc38b10a0
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:09:38 +0100

    feat: add the about page

---
+++ src/pages/About/about.js
@@ -0,0 +1,30 @@
+import styles from "./about.module.css";
+import datas from "../../datas/aboutList.json"
+import Header from "../../components/Header/header";
+import Banner from "../../components/Banner/banner";
+import Footer from "../../components/Footer/footer";
+import Collapse from "../../components/Collapse/collapse"
+
+function About() {
+    return (
+      <div>
+        <div className={styles.container}>
+            <Header />
+            <Banner />
+                <div className={styles.about__body}>
+                    {datas.map(data => {
+                        return (
+                            <Collapse                                
+                                title={data.title}
+                                content={data.content}
+                            />
+                        )
+                    })}
+                </div>          
+        </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default About;
