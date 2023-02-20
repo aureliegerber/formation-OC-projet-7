23c0f11f3570624a6a278e73a399f2fb62a04207
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:05:45 +0100

    feat: add the component Gallery

---
+++ src/components/Gallery/gallery.js
@@ -0,0 +1,24 @@
+import styles from "./gallery.module.css";
+import datas from "../../datas/lodgingList.json"
+import Card from "../Card/card"
+
+function Gallery() {
+    return (
+        <section className={styles.gallery}>
+            <div className={styles.gallery__cards}>
+                {datas.map(data => {
+                    return (
+                        <Card
+                            key={data.id}
+                            id={data.id}
+                            title={data.title}
+                            cover={data.cover}
+                        />
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Gallery;
