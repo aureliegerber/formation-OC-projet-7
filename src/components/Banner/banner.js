8551d0930ae3f6f094d8683d25d4bad849552e98
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-20 14:02:16 +0100

    feat: add the component Banner

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,20 @@
+import styles from "./banner.module.css";
+
+function Banner () {  
+  if (window.location.pathname === "/") {
+    return (
+      <div className = {`${styles.banner} ${styles.banner__home}`}>
+        <h1 className = {styles.banner__title}>Chez vous, partout et ailleurs</h1>
+      </div>
+    )
+  }
+
+  if (window.location.pathname === "/about") {
+    return (
+      <div className = {`${styles.banner} ${styles.banner__about}`}>        
+      </div>
+    )
+  }    
+}
+  
+export default Banner;
