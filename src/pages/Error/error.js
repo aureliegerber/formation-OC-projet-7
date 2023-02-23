891ed371fd62b3fb27d2a6b5022a51fe35fd1e07
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-23 22:53:16 +0100

    refactor: correct spelling mistakes

--- src/pages/Error/error.js
+++ src/pages/Error/error.js
@@ -9,7 +9,7 @@ function Error() {
             <Header />
             <div className={styles.error__body}>
                 <h1 className={styles.error__title}>404</h1>
-                <p className={styles.error__p}>Oups ! La page que vous demandez n'exsite pas</p>
+                <p className={styles.error__p}>Oups ! La page que vous demandez n'existe pas</p>
                 <Link to="/" className={styles.error__toHome}>Retourner sur la page d'accueil</Link>
             </div>
         </div>
