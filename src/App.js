38a2767588a26caf42cf5da5498025e3d5915335
Aurelie Gerber <gerber.prof@gmail.com>
2023-02-27 22:23:26 +0100

    refactor: add comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home";
 import About from "./pages/About/about";
 import Error from "./pages/Error/error";
 import Lodging from "./pages/Lodging/lodging";
 
+/**
+ * App component
+ * @return {React.ReactElement}
+ */
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/lodging/:id" element={<Lodging />} />
         <Route path="*" element={<Error />} />
